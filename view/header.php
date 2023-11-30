61259f49fdbf61c705d972ded7c1616273274e27
Nguyen Thien <nguyenvanthien24032004@gmail.com>
2023-11-30 21:31:17 +0700

    donhang

--- view/header.php
+++ view/header.php
@@ -282,34 +282,9 @@ ob_start();
                                 <li>
                                     <div class="widget woocommerce widget_shopping_cart">
                                         <div class="widget_shopping_cart_content">
-                                            <ul class="woocommerce-mini-cart cart_list product_list_widget ">
-                                                <li class="woocommerce-mini-cart-item mini_cart_item">
-                                                    <a href="#" class="remove" aria-label="Remove this item" data-product_id="65" data-product_sku="">×</a>
-                                                    <a href="single-product-sidebar.html">
-                                                        <img src="assets/images/products/mini-cart1.jpg" class="attachment-shop_thumbnail size-shop_thumbnail wp-post-image" alt="">XONE Wireless Controller&nbsp;
-                                                    </a>
-                                                    <span class="quantity">1 ×
-                                                        <span class="woocommerce-Price-amount amount">
-                                                            <span class="woocommerce-Price-currencySymbol">$</span>64.99</span>
-                                                    </span>
-                                                </li>
-                                                <li class="woocommerce-mini-cart-item mini_cart_item">
-                                                    <a href="#" class="remove" aria-label="Remove this item" data-product_id="27" data-product_sku="">×</a>
-                                                    <a href="single-product-sidebar.html">
-                                                        <img src="assets/images/products/mini-cart2.jpg" class="attachment-shop_thumbnail size-shop_thumbnail wp-post-image" alt="">Gear Virtual Reality 3D with Bluetooth Glasses&nbsp;
-                                                    </a>
-                                                    <span class="quantity">1 ×
-                                                        <span class="woocommerce-Price-amount amount">
-                                                            <span class="woocommerce-Price-currencySymbol">$</span>72.00</span>
-                                                    </span>
-                                                </li>
-                                            </ul>
+                                           
                                             <!-- .cart_list -->
-                                            <p class="woocommerce-mini-cart__total total">
-                                                <strong>Subtotal:</strong>
-                                                <span class="woocommerce-Price-amount amount">
-                                                    <span class="woocommerce-Price-currencySymbol">$</span>136.99</span>
-                                            </p>
+                                           
                                             <p class="woocommerce-mini-cart__buttons buttons">
                                                 <a href="index.php?act=giohang" class="button wc-forward">View cart</a>
                                                 <a href="index.php?act=theodoi" class="button checkout wc-forward">Checkout</a>
